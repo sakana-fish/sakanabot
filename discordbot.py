1b6ce323e51fdb963f2f0066cedb4e1a787bb214
sakana <64359744+sakana-fish@users.noreply.github.com>
2021-05-16 15:53:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -73,7 +73,7 @@ async def suse(ctx): #.sの説明
     
 @client.command()
 async def fish(ctx, about = "🐟🐟🐟 使い方 🐟🐟🐟"):
-  text="`.s` : 交流戦募集開始※再び.sすることでリセット 英語スタンプ→の/補欠/へ\n`.suse` : .sの使い方\n`.l 時間` : 指定時間に挙手した人の名前\n`.m 時間` : 指定時間に挙手した人にメンション\n`.cal` : 即時集計。順位は16進数でも入力可(入力例:`123456`,`1357911`,`789abc`)、`call`or`777`で新着に、`end`で終了、`back`or`333`で一回分だけ修正可能\n`.reset` : 挙手回数カウント、再度.resetでカウントリセット\n`.set 数字` : メモ登録\n`.memo 数字 or 登録名` : メモ呼び出し\n`.ran 数字` : ランダムに数字出力\n`.div 数字 リスト...`: 組み分け(例:`.div 3 a b c d e f g h`)\n`.chs リスト...` : 選択(例:`.chs a b c`)\n`.v/.v2` : 匿名or非匿名アンケート(2択)、募集者の👋で終了\n作成者:さかな(@sakana8dx)\nさかなBot導入: https://discord.com/api/oauth2/authorize?client_id=758555841296203827&permissions=223296&scope=bot"
+  text="`.s` : 交流戦募集開始※再び.sすることでリセット 英語スタンプ→の/補欠/へ\n`.suse` : .sの使い方\n`.l 時間` : 指定時間に挙手した人の名前\n`.m 時間` : 指定時間に挙手した人にメンション\n`.cal` : 即時集計。順位は16進数でも入力可(入力例:`123456`,`1357911`,`789abc`)、`call`or`777`で新着に、`end`で終了、`back`or`333`で一回分だけ修正可能\n`.reset` : 挙手回数カウント、再度.resetでカウントリセット\n`.mkmg (<NGワード>)` : mkmgツイート監視(例: `.mkmg`, `.mkmg 仮入団 海外`) \n`.set 数字` : メモ登録\n`.memo 数字 or 登録名` : メモ呼び出し\n`.ran 数字` : ランダムに数字出力\n`.div 数字 リスト...`: 組み分け(例:`.div 3 a b c d e f g h`)\n`.chs リスト...` : 選択(例:`.chs a b c`)\n`.v/.v2` : 匿名or非匿名アンケート(2択)、募集者の👋で終了\n作成者:さかな(@sakana8dx)\nさかなBot導入: https://discord.com/api/oauth2/authorize?client_id=758555841296203827&permissions=223296&scope=bot"
   help1 = discord.Embed(title=about,color=0xe74c3c,description=text)
   await ctx.send(embed=help1)       
 
